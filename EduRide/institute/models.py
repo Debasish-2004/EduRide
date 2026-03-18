592818c2872cb69884e7c5835cc92cccd1733eb2
Debasish Dash <2004debasishdash@gmail.com>
2026-03-18 10:19:03 +0530

    create_route is now storing data in the database

--- EduRide/institute/models.py
+++ EduRide/institute/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Route(models.Model):
+    bus_no = models.CharField(max_length=50)
+    route_name = models.CharField(max_length=100)
+    coordinates = models.JSONField()
+    waypoints = models.JSONField()
+
+    def __str__(self):
+        return f"{self.bus_no}"
--- EduRide/institute/views.py
+++ EduRide/institute/views.py
@@ -1,11 +1,52 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render,redirect
+from .models import Route
 
 # Create your views here.
+
 def institute_admin(request):
     return render(request, "institute_admin.html")
 
-def buslist(request):
-    return render(request, "buslist.html")
 
+def buslist(request):
+    buses = Route.objects.all().order_by('-id')
+    return render(request, "buslist.html", {
+        "buses": buses
+    })
+
+# the route added and then the page will forward to the buslist when added..
 def route(request):
+    if request.method == "POST":
+        bus_no = request.POST.get("no")
+        route_name = request.POST.get("route_name")
+        coordinates = request.POST.get("coordinates")
+        waypoints = request.POST.get("waypoints")
+
+        if not bus_no or not route_name:
+            return render(request, "create_route.html", {
+                "error": "Bus number and route name are required."
+            })
+
+        if not coordinates or not waypoints:
+            return render(request, "create_route.html", {
+                "error": "Please create a valid route on the map."
+            })
+
+        try:
+            coordinates_data = json.loads(coordinates)
+            waypoints_data = json.loads(waypoints)
+        except json.JSONDecodeError:
+            return render(request, "create_route.html", {
+                "error": "Invalid route data."
+            })
+
+        Route.objects.create(
+            bus_no=bus_no,
+            route_name=route_name,
+            coordinates=coordinates_data,
+            waypoints=waypoints_data
+        )
+
+        return redirect("buslist")   # or any page you want after saving
+
     return render(request, "create_route.html")
